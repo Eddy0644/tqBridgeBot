afa986dbc837c17d4d38e6e62f5824d2dd915aec
netcy <a@nc32.top>
2023-05-24 13:19:57 +0800

    .

--- src/i.js
+++ src/i.js
@@ -1,6 +1,8 @@
+// noinspection JSUnresolvedVariable
+
 const {Bot: MiraiBot, Message: mrMessage} = require('mirai-js');
 const secret = require('../config/secret');
-const userConf = require('../config/userconf');
+// const userConf = require('../config/userconf');
 const {qqLogger, tgLogger, defLogger} = require('./logger')('startup');
 
 const {tgbot} = require('./tgbot-pre');
@@ -14,18 +16,51 @@ const qqBot = new MiraiBot();
 
 async function sendTestMessage() {
     await qqBot.sendMessage({
-        friend: secret.test.targetQNumber,
-        message: new mrMessage().addText('114514')
+        friend: secret.test.targetQNumber, message: new mrMessage().addText('114514')
     });
+}
+
+let msgMappings = [];
+
+function addToMsgMappings(tgMsgId, talker, qqMsg) {
+    msgMappings.push([tgMsgId, talker, qqMsg]);
+    defLogger.trace(`Added temporary mapping from TG msg #${tgMsgId} to QQ ${talker.nickname}`);
+}
+
+async function onTGMsg(tgMsg) {
+    try {
+        if (tgMsg.reply_to_message) {
+            for (const mapPair of msgMappings) {
+                if (mapPair[0] === tgMsg.reply_to_message.message_id) {
+                    await qqBot.sendMessage({
+                        // 好友 qq 号
+                        friend: mapPair[1].id, // Message 实例，表示一条消息
+                        message: new mrMessage().addText(tgMsg.text)
+                    });
+                    await tgbot.sendChatAction(secret.test.targetTGID, "choose_sticker").catch((e) => {
+                        tgLogger.error(e.toString());
+                    });
+                    defLogger.debug(`Handled a message send-back to ${mapPair[2]}.`);
+                    return;
+                }
+            }
+            defLogger.debug(`Unable to send-back due to no match in msgMappings.`);
+        } else {
+            await tgbot.sendMessage(tgMsg.chat.id, 'Nothing to do upon your message, ' + tgMsg.chat.id);
+        }
+    } catch (e) {
+        tgLogger.warn(`Uncaught Error while handling TG message: ${e.message}`);
+    }
 }
 
 async function main() {
     await qqBot.open(secret.miraiCredential);
     // await sendTestMessage();
     qqBot.on('FriendMessage', async data => {
-        await tgbot.sendMessage(secret.test.targetTGID, `Got QQ message from:<code>${JSON.stringify(data.sender, null, 2)}</code> Message Chain is: <code>${JSON.stringify(data.messageChain, null, 2)}</code>`, {
+        const tgMsg = await tgbot.sendMessage(secret.test.targetTGID, `Got QQ message from:<code>${JSON.stringify(data.sender, null, 2)}</code> Message Chain is: <code>${JSON.stringify(data.messageChain, null, 2)}</code>`, {
             parse_mode: "HTML"
         });
+        addToMsgMappings(tgMsg, data.sender, data.messageChain);
         // await qqBot.sendMessage({
         //     friend: data.sender.id,
         //     message: new mrMessage().addText('Echo !'),
@@ -33,4 +68,5 @@ async function main() {
     });
 }
 
-main().then(r => console.log(r));
+tgbot.on('message', onTGMsg);
+main().then(r => defLogger.info(`Bootstrap completed. (${r})`));
--- src/tgbot-pre.js
+++ src/tgbot-pre.js
@@ -7,24 +7,24 @@ process.env["NTBA_FIX_350"] = "1";
 let tgbot;
 if (isPolling) {
     tgbot = new TelegramBot(secret.tgCredential.token,
-        {polling: {interval: 2000}, request: {proxy: require("../proxy")},});
+        {polling: {interval: 4000}, request: {proxy: require("../proxy")},});
     tgbot.deleteWebHook();
 } else {
-    tgbot = new TelegramBot(secretConfig.botToken, {
-        webHook: {
-            port: 8443,
-            max_connections: 3,
-            healthEndpoint: "/health",
-            key: "config/srv.pem",
-            cert: "config/cli.pem",
-        },
-        request: {proxy: require("../proxy")}
-    });
-    tgbot.setWebHook(`${secretConfig.webHookUrlPrefix}${process.argv[3]}/bot${secretConfig.botToken}`, {
-        drop_pending_updates: true
-        /* Please, remove this line after the bot have ability to control messages between instances!!! */
-    });
-    tgbot.openWebHook();
+    // tgbot = new TelegramBot(secretConfig.botToken, {
+    //     webHook: {
+    //         port: 8443,
+    //         max_connections: 3,
+    //         healthEndpoint: "/health",
+    //         key: "config/srv.pem",
+    //         cert: "config/cli.pem",
+    //     },
+    //     request: {proxy: require("../proxy")}
+    // });
+    // tgbot.setWebHook(`${secretConfig.webHookUrlPrefix}${process.argv[3]}/bot${secretConfig.botToken}`, {
+    //     drop_pending_updates: true
+    //     /* Please, remove this line after the bot have ability to control messages between instances!!! */
+    // });
+    // tgbot.openWebHook();
 }
 
 module.exports = {
