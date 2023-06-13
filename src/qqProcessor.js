9eaf05c2894c1310b6eb1c9f4bc5c91ff46b5022
netcy <a@nc32.top>
2023-06-13 10:34:28 +0800

    fix. format issue

--- src/qqProcessor.js
+++ src/qqProcessor.js
@@ -7,19 +7,20 @@ async function a() {
 
 function handleForwardMessage(nodeList, indent = 1) {
     const {qqLogger} = env;
-    let ans, contentAll = "", peopleList = [], firstTime = null;
+    let ans, contentAll = "", peopleList = [], firstTime = null, talkerStr = "";
     const prodInd = (ind) => {
         let content = "";
-        for (let i = 0; i < indent; i++) content += "--";
+        for (let i = 0; i < ind; i++) content += "---";
         return content;
     };
     for (const node of nodeList) {
-        if (!firstTime) firstTime = dayjs.unix(node.time).format("MMDD HHmmss");
+        if (!firstTime) firstTime = dayjs.unix(node.time).format("MMDD HH:mm:ss");
         let content = prodInd(indent);
         if (!peopleList.includes(node.senderId)) {
-            peopleList.push([node.senderId, node.senderName]);
+            peopleList.push(node.senderId);
+            talkerStr += `/f${node.senderId} == ${node.senderName}\n`;
         }
-        content += `[${node.senderName}]➡️`;
+        content += `[${node.senderName}]➡ `;
         for (const msg of node.messageChain) switch (msg.type) {
             case "Plain":
                 content += msg.text + ` `;
@@ -33,13 +34,9 @@ function handleForwardMessage(nodeList, indent = 1) {
             default:
                 qqLogger.debug(`Unparsed MessageType: (${msg.type}) In a ForwardMessage. Ignored.`);
         }
-        contentAll += content;
+        contentAll += content + "\n";
     }
-    let talkerStr = "";
-    for (const oneTalker of peopleList) {
-        talkerStr += `/f${oneTalker[0]} == ${oneTalker[1]}\n`;
-    }
-    ans = `${prodInd(indent - 1)}[Forwarded]\nFirst Msg Time:${firstTime}\nTalkers:[${talkerStr}${contentAll}`;
+    ans = `${prodInd(indent - 1)}[Forwarded]\nFirst Msg Time:${firstTime}\nTalkers:${talkerStr}${contentAll}`;
     qqLogger.trace(`Parsed ForwardMessage:\n${ans}`);
     return ans;
 }
